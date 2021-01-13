8c0d1e7072567789f48bcfb4bc74199b12f78bec
Bryan Hill <hillbj1@appstate.edu>
2021-01-13 16:19:37 -0500

    Updated trending page.

--- js/apiLookup.js
+++ js/apiLookup.js
@@ -1,27 +1,28 @@
-const base_url = 'https://api.jikan.moe/v3/search/anime?q=';
+const base_url = 'https://api.jikan.moe/v3/';
+const search_endpoint = 'search/anime?q=';
 addActionListeners();
 
 //Pulling json data from api
 function queryAPI(query) {
-  fetch(base_url + query)
+  fetch(base_url + search_endpoint + query)
   .then(response => response.json())
   .then(response => response.results)
-  .then(data => processResponse(data));
+  .then(data => processResponseSearch(data));
 }
 
-function processResponse(response) {
-  removeContent();
+function processResponseSearch(response) {
+  removeContentSearch();
   response.forEach(result => {
-    addCard(result);
+    addCardSearch(result);
   });
 }
 
-function removeContent() {
+function removeContentSearch() {
   $('.landing').remove();
   $('.card').remove();
 }
 
-function addCard(result) {
+function addCardSearch(result) {
   $('.cards').append(
     '<div class="card" style="width: 18rem;"> \
       <img class="card-img-top" src="' + result.image_url + '" alt="Card image cap"> \
---
+++ js/trending.js
@@ -0,0 +1,32 @@
+const trending_endpoint = 'top/anime/1/airing';
+
+$( window ).on('load', function() {
+  fetch(base_url + trending_endpoint)
+  .then(response => response.json())
+  .then(response => response.top)
+  .then(data => processResponseTrending(data));
+});
+
+function processResponseTrending(response) {
+  removeContentTrending();
+  response.forEach(result => {
+    addCardTrending(result);
+  });
+}
+
+function removeContentTrending() {
+  $('.card').remove();
+}
+
+function addCardTrending(result) {
+  $('.cards').append(
+    '<div class="card" style="width: 18rem;"> \
+      <img class="card-img-top" src="' + result.image_url + '" alt="Card image cap"> \
+      <div class="card-body"> \
+        <h5 class="card-title">' + result.title + '</h5> \
+        <h5 class="card-title"> Rank: ' + result.rank + '</h5> \
+        <p class="card-text">Score: ' + result.score + '</p> \
+        <a href="' + result.url + '" class="btn btn-primary">More Information</a> \
+      </div> \
+    </div>');
+}
