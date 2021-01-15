433290fe53d8f551d799cbcc89db8e9b7ab7419d
HillBryan <hillbj1@appstate.edu>
2021-01-15 14:46:33 -0500

    Added cards redirect functionality.

--- js/info.js
+++ js/info.js
@@ -1,2 +1,45 @@
 //Get mal_id with jquery
 //use anime/{id} endppint to get everything
+const anime_search_by_id_endpoint = 'anime/'
+
+
+$(window).on('load', function() {
+  deleteOldData();
+  console.log(sessionStorage.getItem('id'));
+  loadData(sessionStorage.getItem('id'));
+});
+
+function deleteOldData() {
+  $('jumbotron-item').remove();
+}
+
+function loadData(id) {
+  fetch(base_url + anime_search_by_id_endpoint + id)
+  .then(response => response.json())
+  .then(response => {
+    fillJumbotronHeader(response);
+    fillJumbotronBody(response);
+  });
+}
+
+function fillJumbotronHeader(response) {
+  $('.jumbotron-header').append(
+    '<img class="jumbotron-img class="jumbotron-item"" src=' +
+    response.image_url + ' alt="Card image cap">'
+  );
+}
+
+function fillJumbotronBody(response) {
+  $('.jumbotron-body').append(' \
+    <h1 class="jumbotron-item">' + response.title + '</h1><hr></hr> \
+    <p class="jumbotron-item">' + response.synopsis + '</p> \
+    <h5 class="jumbotron-item">Episodes: ' + (response.episodes ?
+      response.episodes : 'Unknown') + '</h5> \
+    <h5 class="jumbotron-item">Rating: ' + response.rating + '</h5> \
+    <h5 class="jumbotron-item">Status: ' + response.status + '</h5> \
+    <h5 class="jumbotron-item">Duration: ' + response.aired.string + '</h5> \
+    <h5 class="jumbotron-item">Score: ' + response.score + '</h5> \
+    <h5 class="jumbotron-item">Rank: ' + response.rank + '</h5> \
+    <h5 class="jumbotron-item">Broadcast Time: ' + response.broadcast + '</h5> \
+  ');
+}
--- js/trending.js
+++ js/trending.js
@@ -12,6 +12,7 @@ function processResponseTrending(response) {
   response.forEach(result => {
     addCardTrending(result);
   });
+  addCardFunction();
 }
 
 function removeContentTrending() {
@@ -20,15 +21,28 @@ function removeContentTrending() {
 
 function addCardTrending(result) {
   $('.cards').append(
-    '<div class="card" style="width: 18rem;"> \
-      <img class="card-img-top standard" src="' + result.image_url + '" alt="Card image cap"> \
-      <div class="card-body"> \
-        <h5 class="card-title large">' + result.title + '</h5> \
-        <p class="card-title"> <strong>Rank: ' + result.rank + '</strong></p> \
-        <p class="card-text">Score: ' + result.score + ' / 10</p> \
+    '<div class="card" style="width: 18rem;" id=' + result.mal_id + '> \
+      <img class="card-img-top standard" src="' + result.image_url + '" alt=" \
+      Card image cap" id=' + result.mal_id + '> \
+      <div class="card-body" id=' + result.mal_id + '> \
+        <h5 class="card-title large" id=' + result.mal_id + '>' + result.title +
+        '</h5> \
+        <p class="card-title" id=' + result.mal_id + '> <strong id=' +
+        result.mal_id + '>Rank: ' + result.rank + '</strong></p> \
+        <p class="card-text" id=' + result.mal_id + '>Score: ' + result.score +
+        ' / 10</p> \
       </div> \
-      <div class="card-footer"> \
-        <a href="' + result.url + '" class="btn btn-primary">More Information</a> \
+      <div class="card-footer" id=' + result.mal_id + '> \
+        <a href="' + result.url + '" class="btn btn-primary" id=' +
+        result.mal_id + '>More Information</a> \
       </div> \
     </div>');
+}
+
+function addCardFunction() {
+  //Cards click to go to info page
+  $('.card').on('click', function() {
+    sessionStorage.setItem('id', $(event.target).attr('id'));
+    window.location = '../html/info.html';
+  });
 }
