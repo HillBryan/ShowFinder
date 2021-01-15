6910101d1e3088cc8d0fe296f131671117c25533
HillBryan <hillbj1@appstate.edu>
2021-01-15 15:56:56 -0500

    Added full site info abilities.

--- js/genres.js
+++ js/genres.js
@@ -49,6 +49,8 @@ function processResponseGenre(response) {
   response.forEach(result => {
     addCardGenre(result);
   });
+
+  addCardFunction();
 }
 
 function removeContentGenre() {
@@ -57,15 +59,26 @@ function removeContentGenre() {
 
 function addCardGenre(result) {
   $('.cards').append(
-    '<div class="card" style="width: 18rem;"> \
-      <img class="card-img-top standard" src="' + result.image_url + '" alt="Card image cap"> \
-      <div class="card-body"> \
-        <h5 class="card-title large">' + result.title + '</h5> \
-        <p class="card-title"> Episodes: ' + result.episodes + '</p> \
-        <p class="card-text">Score: ' + result.score + ' / 10</p> \
+    '<div class="card" style="width: 18rem;" id=' + result.mal_id + '> \
+      <img class="card-img-top standard" src="' + result.image_url + '" \
+      alt="Card image cap" id=' + result.mal_id + '> \
+      <div class="card-body" id=' + result.mal_id + '> \
+        <h5 class="card-title large" id=' + result.mal_id + '>' + result.title + '</h5> \
+        <p class="card-title" id=' + result.mal_id + '> Episodes: ' + result.episodes + '</p> \
+        <p class="card-text" id=' + result.mal_id + '>Score: ' + result.score + ' / 10</p> \
       </div> \
-      <div class="card-footer"> \
-        <a href="' + result.url + '" class="btn btn-primary">More Information</a> \
+      <div class="card-footer" id=' + result.mal_id + '> \
+        <a href="' + result.url + '" class="btn btn-primary" id=' +
+        result.mal_id + '>More Information</a> \
       </div> \
     </div>');
+}
+
+
+function addCardFunction() {
+  //Cards click to go to info page
+  $('.card').on('click', function() {
+    sessionStorage.setItem('id', $(event.target).attr('id'));
+    window.location = '../html/info.html';
+  });
 }
--- js/results.js
+++ js/results.js
@@ -4,22 +4,34 @@ $(window).on('load', function() {
 
 function loadCards(response) {
   $('.card').remove();
-  console.log(response);
+
   response.forEach(result => {
     addCardSearch(result);
   });
+
+  addCardFunction();
 }
 
 function addCardSearch(result) {
   $('.cards').append(
-    '<div class="card" style="width: 18rem;"> \
-      <img class="card-img-top standard" src="' + result.image_url + '" alt="Card image cap"> \
-      <div class="card-body"> \
-        <h5 class="card-title">' + result.title + '</h5> \
-        <p class="card-text">' + result.synopsis + '</p> \
+    '<div class="card" style="width: 18rem; id=' + result.mal_id + '"> \
+      <img class="card-img-top standard" src="' + result.image_url + '" \
+      id=' + result.mal_id + ' alt="Card image cap"> \
+      <div class="card-body" id=' + result.mal_id + '> \
+        <h5 class="card-title" id=' + result.mal_id + '>' + result.title + '</h5> \
+        <p class="card-text" id=' + result.mal_id + '>' + result.synopsis + '</p> \
       </div> \
-      <div class="card-footer"> \
-        <a href="' + result.url + '" class="btn btn-primary">More Information</a> \
+      <div class="card-footer" id=' + result.mal_id + '> \
+        <a href="' + result.url + '" class="btn btn-primary" id=' +
+        result.mal_id + '>More Information</a> \
       </div> \
     </div>');
+}
+
+function addCardFunction() {
+  //Cards click to go to info page
+  $('.card').on('click', function() {
+    sessionStorage.setItem('id', $(event.target).attr('id'));
+    window.location = '../html/info.html';
+  });
 }
